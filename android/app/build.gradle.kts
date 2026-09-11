0fb2ee2c8f1c5e728461a72c3b80b13e937a2113
gitlokesh1 <gitlokesh@gmail.com>
2026-09-11 23:34:42 +0530

    build(android): apply Google Services Gradle plugin for Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 val portalUrl = providers.gradleProperty("portalUrl").orElse("").get().trim().trimEnd('/')
